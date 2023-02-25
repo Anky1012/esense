37f86fc53d738f5b74cadd0d9b3ff03f3aa37ae8
anky <ankit.makde16@gmail.com>
2023-02-25 17:15:55 +0530

    change

--- employeedata/enroll/urls.py
+++ employeedata/enroll/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.employee_create, name='employee_create'),
     path('employee_list',views.employee_list, name='employee_list'),
+    
     path('employee_detail/<int:id>',views.employee_detail, name='employee_detail'),
-    path('home/',views.home),
+   
     
 ]   + static (settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- employeedata/enroll/views.py
+++ employeedata/enroll/views.py
@@ -1,25 +1,11 @@
-from django.shortcuts import render,HttpResponse,redirect,get_object_or_404
+from django.shortcuts import render,redirect
 from django.views import View
 from .forms import EmployeeRegistrationForm
 from .models import Employee
 
-# Create your views here.
-# class EmployeeRegistrationView(View):
-#   def get(self,request):
-#      form = EmployeeRegistrationForm()
-#      return render(request,'app/registration.html',{'form': form}) 
-#   def post(self,request):
-#       form = EmployeeRegistrationForm(request.POST)
-#       # print(form)
-#       if form.is_valid():
-#          # print(form.is_valid())
-#           form.save()
-#           form = EmployeeRegistrationForm()
-#       return render(request,'app/registration.html',{'form': form})  
-  
 
-def home(request):
-   return HttpResponse('hello')
+
+
 def employee_create(request):
     if request.method == 'POST':
         form = EmployeeRegistrationForm(request.POST, request.FILES)
@@ -37,9 +23,7 @@ def employee_list(request):
 
 
 def employee_detail(request,id):
-    # employee =get_object_or_404(Employee, id=id)
-    # employee_id = request.GET.get('employee_id')
-    employee = Employee.objects.filter(id=id)
-    # employee= get_object_or_404(Employee, pk=image_id).image_field
     
+    employee = Employee.objects.filter(id=id)
+        
     return render(request, 'app/employeedetail.html', {'employee': employee})
